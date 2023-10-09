8a61537f572734d2188142db13f05dd5b77f7347
EMHMoloney <>
2023-10-09 10:52:02 +0000

    Add JavaScript framework-function,eventListener and initial variables required lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,27 @@
+//starts quiz after DOCLoadingComplete
+function loadQuestion (){
 
+};
+//handles 
+function handleTueButtonClick(){
 
+};
+function handleFalseButtonclick(){
+
+};
+
+function showResults(){
+
+};
+//score variable
+let score = 0;
+//array of question:answer objects
+let questions = [
+];
+//event listeners required
+addEventListener("click", handleTrueButtonClick);
+addEventListener("click", handleFalseButtonClick);
+addEventListener("DOMContentLoaded", ());
+//username handle
+let username = "";
+document.getElementById("username");
