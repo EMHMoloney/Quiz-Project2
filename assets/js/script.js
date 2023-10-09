bf05003d12f20c2cbdb075dca3fe1f5a1e62c737
EMHMoloney <>
2023-10-09 13:42:33 +0000

    Add array for questions in quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 //starts quiz after DOCLoadingComplete
+addEventListener("DOMContentLoaded", function () {
+ let username = "";
+
 function loadQuestion (){
 
 };
 //handles 
-function handleTueButtonClick(){
+function handleTrueButtonClick(){
 
 };
-function handleFalseButtonclick(){
+function handleFalseButtonClick(){
 
 };
 
@@ -15,13 +18,23 @@ function showResults(){
 };
 //score variable
 let score = 0;
-//array of question:answer objects
+//questions array of question:value, answer:value objects
 let questions = [
+    { question: "True or False:Irish film 'An Irish Goodbye'won the Oscar for Best Short Film. " , answer:true },
+    { question: "True or False:The 2023 Rugby World Cup hosts are South Africa. ", answer: false},
+    { question: "True or False: Michael D. Higgins and Leo Varadkar became the first ever President and Taoiseach to attend a British coronation.  ", answer: true },
+    { question: "True or False: An Post moved is headquarters from the G.P.O to Cork.", answer: false },
+    { question: "True or False: Ireland was represented in the Eurovision Song Contest 2023 by Hozier", answer: false },
+    { question: "True or False: Limerck won the All Ireland senior hurling final for the fourth time in a row", answer: true },
+    { question: "True or False: U.S. Vice President Kamala Harris made a state visit to Ireland in April", answer: false },
+    { question: "True or False: Meta was fined €390 million by the Irish Data Protection Council", answer: true },
+    { question: "True or False: Comedian Patrick Kielty took over as the new host of The Late Late Show", answer: true },
+    { question: "True or False: The Irish Grand National was won by 'I Am Roy Keane', trained by Willie Mullins", answer: false },
 ];
 //event listeners required
 addEventListener("click", handleTrueButtonClick);
 addEventListener("click", handleFalseButtonClick);
-addEventListener("DOMContentLoaded", ());
 //username handle
-let username = "";
 document.getElementById("username");
+
+);
